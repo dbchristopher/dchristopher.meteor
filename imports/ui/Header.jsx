a577e3d875f1feddb6ac54b0d8ca7a43b49f7551
Daniel Christopher <dbchristopher@gmail.com>
2017-08-28 17:02:41 -0700

    initial meteor + jsx repo

---
+++ imports/ui/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class componentName extends Component {
+  render() {
+    return (
+      <header className="header">
+        <canvas className="header__canvas"></canvas>
+        <h1>Daniel Christopher</h1>
+      </header>
+    );
+  }
+}
+
+export default componentName;
