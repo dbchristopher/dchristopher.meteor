53eaecdb704bf624cd81db70e4833cbe4160febb
Daniel Christopher <dbchristopher@gmail.com>
2017-08-29 10:03:16 -0700

    style canvas element

--- imports/ui/Header.jsx
+++ imports/ui/Header.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class componentName extends Component {
+  constructor(props) {
+    super(props);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+  }
+
   render() {
     return (
       <header className="header">
-        <canvas className="header__canvas"></canvas>
+        <canvas className="header__canvas" ref={(el) => { this.canvas = el; }}></canvas>
         <h1>Daniel Christopher</h1>
       </header>
     );
