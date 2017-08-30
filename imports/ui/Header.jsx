196875243f95f067af3f4fa35ef81e302f2e77d6
Daniel Christopher <dbchristopher@gmail.com>
2017-08-30 11:18:00 -0700

    changing shapes

--- imports/ui/Header.jsx
+++ imports/ui/Header.jsx
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 
-const dimensions = { w: 30, h: 60 };
+const dimensions = { w: 40, h: 40 };
 const origin = { x: 0, y: 0 };
-const margin = 40;
+const margin = 5;
 
 function drawTriangle(ctx, origin) {
   ctx.beginPath();
-  ctx.globalCompositeOperation = "lighter";
-  ctx.fillStyle = 'RGBA(19, 124, 201, .10)';
-  ctx.moveTo(origin.x, origin.y);
-  ctx.lineTo(origin.x + dimensions.w, origin.y + (dimensions.h/2));
-  ctx.lineTo(origin.x, origin.y + dimensions.h)
+  ctx.globalCompositeOperation = "screen";
+  ctx.fillStyle = 'RGBA(19, 124, 201, .3)';
+  ctx.rect(origin.x,origin.y,dimensions.w,dimensions.h);
   ctx.fill();
   ctx.closePath();
 }
 
 function drawTriangleLeft(ctx, origin) {
   ctx.beginPath();
-  ctx.globalCompositeOperation = "lighter";
-  ctx.fillStyle = 'RGBA(19, 124, 201, .10)';
-  ctx.moveTo(origin.x, origin.y);
-  ctx.lineTo(origin.x - dimensions.w, origin.y + (dimensions.h/2));
-  ctx.lineTo(origin.x, origin.y + dimensions.h)
+  ctx.globalCompositeOperation = "screen";
+  ctx.fillStyle = 'RGBA(19, 124, 201, .5)';
+  ctx.arc(origin.x, origin.y, dimensions.w, 0, 2 * Math.PI, false);
   ctx.fill();
   ctx.closePath();
 }
@@ -35,7 +31,7 @@ function fillFrame(canvas, origin, shape) {
   _.times(cols + 2, (c) => {
     const hOrigin = { x: origin.x + ((dimensions.w + margin) * c) - dimensions.w, y: origin.y };
     shape(ctx, hOrigin);
-    _.times(rows-1, (r) =>  {
+    _.times(rows, (r) =>  {
       const vOrigin = { x: hOrigin.x, y: hOrigin.y + ((dimensions.h + margin) * r) + dimensions.h + margin};
       shape(ctx, vOrigin);
     });
@@ -44,7 +40,7 @@ function fillFrame(canvas, origin, shape) {
 
 function addBackground(canvas) {
   const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "#0D6BB0";
+  ctx.fillStyle = "#005BA0";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
@@ -56,7 +52,7 @@ function animate(canvas, origin) {
   fillFrame(canvas, origin, drawTriangle);
   fillFrame(canvas, {x: origin.x * -1, y: origin.y - (dimensions.h / 2 + margin / 2) }, drawTriangleLeft);
 
-  const originOffset = { x: (origin.x + .25) % (dimensions.w + margin), y: origin.y}
+  const originOffset = { x: (origin.x + .55) % (dimensions.w + margin), y: origin.y}
   const callback = _.partial(animate, canvas, originOffset, dimensions);
   requestAnimationFrame(callback);
 }
